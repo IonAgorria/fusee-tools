374931f618149ce6f72a48b52cff3acd363c045f
Svyatoslav Ryhel <clamor95@gmail.com>
2023-12-27 13:29:36 +0200

    payloads: implement proper version of dump SBK
    Payload will return SBK in form of 4 u32 hex numbers and
    will reboot the device.
    
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

---
+++ payloads/dump_sbk_usb.c
@@ -0,0 +1,40 @@
+#include <stdint.h>
+#include "soc.h"
+#include "common.h"
+#include "pmc_reset.h"
+#include "strings.h"
+#include "bootrom_usb_helper.h"
+
+#define FUSE_PRIVATE_KEY0 (0x7000F9A4)
+
+uint32_t swap_endian(uint32_t input)
+{
+	uint8_t group[4];
+	int32_t i;
+
+	for (i = 0; i < 4; i++)
+		group[i] = input >> i * 8;
+
+	return group[0] << 24 | group[1] << 16 |
+	       group[2] <<  8 | group[3] <<  0;
+}
+
+void main()
+{
+	char buf[4096];
+
+	uint32_t sbk[4];
+	int32_t i;
+	
+	for (i = 0; i < 4; i++) {
+		sbk[i] = reg_read(FUSE_PRIVATE_KEY0, i * 4);
+		sbk[i] = swap_endian(sbk[i]);
+	}
+
+	sprintf(buf, "Dumped SBK 0x%08x 0x%08x 0x%08x 0x%08x\n",
+		sbk[0], sbk[1], sbk[2], sbk[3]);
+
+	usb_transfer_data(buf, strlen(buf));
+
+	pmc_reset(0);
+}
