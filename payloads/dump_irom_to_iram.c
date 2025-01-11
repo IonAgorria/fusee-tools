a61fa701076c45a247ab2ea502c3ebfb7aab69b3
Ion Agorria <ion@agorria.com>
2025-01-11 01:25:22 +0100

    Add payloads to dump sbk and bootrom to IRAM

---
+++ payloads/dump_irom_to_iram.c
@@ -0,0 +1,19 @@
+#define NO_MAIN_WORKAROUND
+#include "common.h"
+#include "soc.h"
+#include "pmc_reset.h"
+
+void main()
+{
+    uint32_t* src = (uint32_t*) BOOTROM_START;
+    uint32_t* dst = (uint32_t*) 0x40009000;
+	*((uint32_t*) (dst - 2)) = 0xC0DECAFE;
+	*((uint32_t*) (dst - 1)) = 0x00FF0200;
+    uint32_t blocks = (64 * 1024) / 4;
+    
+    for (uint32_t i = 0; i < (0x18000 / 4); i++) {
+        dst[i] = i < blocks ? src[i] : 0;
+    }
+
+    pmc_reset();
+}
---
+++ payloads/dump_sbk_to_iram.c
@@ -0,0 +1,17 @@
+#define NO_MAIN_WORKAROUND
+#include "common.h"
+#include "pmc_reset.h"
+
+void main()
+{
+    uint32_t* sbk = (uint32_t*) 0x40010000;
+	*((uint32_t*) (sbk - 2)) = 0xC0DECAFE;
+	*((uint32_t*) (sbk - 1)) = 0x00FF0100;
+
+    for (uint32_t i = 0; i < 4; i++) {
+        sbk[i] = reg_read(FUSE_PRIVATE_KEY0, i * 4);
+        sbk[i + 4] = swap_endian_32(sbk[i]);
+    }
+    
+    pmc_reset();
+}
