9c301796646b200308b74786d835deab00bafb2c
Ion Agorria <ion@agorria.com>
2025-01-14 19:13:57 +0100

    Add sleep to test usb

--- payloads/test_usb.c
+++ payloads/test_usb.c
@@ -1,11 +1,15 @@
 #include "common.h"
 #include "bootrom_usb_helper.h"
 #include "pmc_reset.h"
+#include "sleep.h"
 
 void main() {
 	char str[] = "Hello, this is a message over USB :)";
-    
-	usb_transfer_data(str, sizeof(str));
+
+	for (int i = 0; i < 12; ++i) {
+		usb_transfer_data(str, sizeof(str));
+		msleep(5000);
+	}
 
     pmc_reset_rcm();
 }
