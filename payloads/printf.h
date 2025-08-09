3f5a9680f39ab4348d56ac4e8287fe2142391e33
Ion Agorria <ion@agorria.com>
2025-08-09 19:04:37 +0200

    Fix printf.h and separate func

--- payloads/printf.h
+++ payloads/printf.h
@@ -1,8 +1,8 @@
 #ifndef PRINTF_H
 #define PRINTF_H
 
-static void putc(int c, void *stream) {
-	UART_PUTC(c);
+static void printf_putc(int c, void *stream) {
+	uart_putc(c);
 }
 
 static void printf(const char *fmt, ...) {
@@ -10,25 +10,8 @@ static void printf(const char *fmt, ...) {
 	uart_init();
 
 	va_start(ap, fmt);
-	kvprintf(fmt, uart_putc, (void*)1, 10, ap);
+	kvprintf(fmt, printf_putc, (void*)1, 10, ap);
 	va_end(ap);
-}
-
-static void dump_memory(uint8_t *src, uint32_t size) {
-	printf("Dump %u Bytes from %p:\r\n", size, src);
-	printf("            00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F\r\n");
-	
-	for (uint32_t i = 0; i < size; i++) {
-		if (i % 16 == 0) {
-			printf("%p: ", &src[i]);
-		}
-		
-		printf("%02x ", src[i]);
-		
-		if (i % 16 == 15) {
-			printf("\r\n");
-		}
-	}
 }
 
 #endif //PRINTF_H
